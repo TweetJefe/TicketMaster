ae1c2ec1de5de037a5b3bd882897d7cfaa9364c9
Tweet Jefe <a.chavychalov@gmail.com>
2026-07-26 00:47:29 +0200

    test: covered OrderServiceImpl with JUnit tests

---
+++ booking-service/src/test/java/com/ticket/master/booking/BookingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ticket.master.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookingApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
